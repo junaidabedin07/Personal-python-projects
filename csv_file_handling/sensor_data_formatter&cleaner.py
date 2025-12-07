8ed2bfe02322242a543398fab729507d2c68c3e1
junaidabedin07 <junaidabedin07@gmail.com>
2025-12-07 17:53:36 +0530

    initial commit

--- csv_file_handling/sensor_data_formatter&cleaner.py
+++ csv_file_handling/sensor_data_formatter&cleaner.py
@@ -3,8 +3,20 @@
 #Learn: CSV, regex, data cleaning
 
 import re
-import csv
+#import csv
 import pandas as pd
+
+#function to extract temperature as float
+def extract_temp(x):
+    if pd.isna(x):
+        return None
+
+    s = str(x).lower()
+    match = re.search(r"[-+]?\d*\.?\d+", s)
+
+    if match:
+        return float(match.group())
+    return None
 
 with open("sectionA_problem1_messy_sensor_logs.csv") as csvfile:
     #csv1 = csv.DictReader(csvfile, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
@@ -20,7 +32,8 @@ with open("sectionA_problem1_messy_sensor_logs.csv") as csvfile:
     new_df.to_csv("cleaned_data.csv", index=False)
 
     #converting temperature from strings to float using regex
-
+    #using apply function we can apply one function to the whole column
+    new_df['temp_C'] = new_df['temp_C'].apply(extract_temp)
 
     #fixing some of the data
     new_df['temp_C'] = (new_df['temp_C']*9/5)+32
